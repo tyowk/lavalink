a5dc8d4eb0a51aaf01fbe414d361afe7d5247e98
Tyowk <tyowk.personal@gmail.com>
2024-11-13 20:24:27 +0700

    Update playTrack.js

--- src/functions/playTrack.js
+++ src/functions/playTrack.js
@@ -1,3 +1,16 @@
 module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    const [query, type] = data.inside.splits;
+    if (!query) return d.aoiError.fnError(d, "custom", {}, `Please provide the title or link of the song you want to play!`);
+
+
+
+
+
+
     
+    return {
+        code: d.util.setCode(data)
+    }
 }
