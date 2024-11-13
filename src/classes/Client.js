7e0b757120be254b1ce9ecc641481d9534f71e09
Tyowk <tyowk.personal@gmail.com>
2024-11-13 22:15:43 +0700

    Update Client.js

--- src/classes/Client.js
+++ src/classes/Client.js
@@ -5,9 +5,11 @@ const { CustomFunctions } = require('./Functions.js');
 exports.MusicClient = class Client extends Shoukaku {
     constructor(client, options) {
         if (!options || !options.nodes) throw new Error('There is no nodes provided to connect on!');
-        if (options.nodes) options.nodes = Array.isArray(options.nodes) ? options.nodes : [options.nodes];
+        options.nodes = Array.isArray(options.nodes) ? options.nodes : [options.nodes];
         if (!options.maxQueueSize) options.maxQueueSize = 100;
         if (!options.maxPlaylistSize) options.maxPlaylistSize = 100;
+        if (!options.searchEngine) options.searchEngine = 'ytsearch';
+        if (!options.debug) options.debug = false;
         
         super(new Connectors.DiscordJS(client), options.nodes, {
             moveOnDisconnect: false,
