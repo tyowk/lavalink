bc1d843cb2bac8c9c3a5c22ace189006069cb7e8
Tyowk <tyowk.personal@gmail.com>
2024-11-13 20:48:38 +0700

    Update Client.js

--- src/classes/Client.js
+++ src/classes/Client.js
@@ -6,6 +6,9 @@ exports.MusicClient = class Client extends Shoukaku {
     constructor(client, options) {
         if (!options || !options.nodes) throw new Error('There is no nodes provided to connect on!');
         if (options.nodes) options.nodes = Array.isArray(options.nodes) ? options.nodes : [options.nodes];
+        if (!options.maxQueueSize) options.maxQueueSize = 100;
+        if (!options.maxPlaylistSize) options.maxPlaylistSize = 100;
+        
         super(new Connectors.DiscordJS(client), options.nodes, {
             moveOnDisconnect: false,
             resume: false,
@@ -20,6 +23,7 @@ exports.MusicClient = class Client extends Shoukaku {
         });
         this.client = client;
         this.client.shoukaku = this;
+        this.client.musicOptions = options;
         this.client.queue = new ClientQueue(this, options);
         new CustomFunctions(this.client, options.debug || false);
         this.on('ready', (name, reconnected) => this.emit(reconnected ? 'reconnect' : 'connect', name));
