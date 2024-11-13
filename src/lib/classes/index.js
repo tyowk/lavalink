7054d5192fac2b09e952e6ef8657a9f8adac831a
Tyowk <tyowk.personal@gmail.com>
2024-11-13 19:43:02 +0700

    Delete src/lib/classes/index.js

--- src/lib/classes/index.js
+++
@@ -1,8 +0,0 @@
-const { Shoukaku } = require('./Shoukaku.js');
-
-exports.Music = class Music extends Shoukaku {
-    constructor(client, options) {
-        super(client, options);
-        
-    }
-}
