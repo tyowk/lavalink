8cf93f9bbecab7c3e4de88ebad0de9ce00a94a91
Tyowk <tyowk.personal@gmail.com>
2024-11-13 20:44:56 +0700

    Update playTrack.js

--- src/functions/playTrack.js
+++ src/functions/playTrack.js
@@ -1,133 +1,55 @@
 const { LoadType } = require('shoukaku');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
-    const [query, type] = data.inside.splits;
+    let [query, type] = data.inside.splits;
     if (!query) return d.aoiError.fnError(d, "custom", {}, `Please provide the title or link of the song you want to play!`);
-
-
-
+    type = type?.toLowerCase()?.replace('youtube', 'ytsearch')?.replace('spotify', 'spsearch')?.replace('soundcloud', 'scsearch');
 
     let player = d.client.queue.get(d.guild.id);
-    const vc = d.member;
     if (!player)
        player = await d.client.queue.create(
-           ctx.guild,
-           vc.voice.channel,
-           ctx.channel
+           d.guild,
+           d.member?.voice?.channel,
+           d.channel
        );
-    const res = await this.client.queue.search(query);
-    const embed = this.client.embed();
+    
+    const res = await d.client.queue.search(query, type);
+    const embed = new EmbedBuilder();
     switch (res.loadType) {
-      case LoadType.ERROR:
-        ctx.sendMessage({
-          embeds: [
-            embed
-              .setColor(this.client.color.red)
-              .setDescription("There was an error while searching."),
-          ],
-        });
-        break;
-      case LoadType.EMPTY:
-        ctx.sendMessage({
-          embeds: [
-            embed
-              .setColor(this.client.color.red)
-              .setDescription("There were no results found."),
-          ],
-        });
-        break;
-      case LoadType.TRACK: {
-        const track = player.buildTrack(res.data, ctx.author);
-        if (player.queue.length > client.config.maxQueueSize)
-          return await ctx.sendMessage({
-            embeds: [
-              embed
-                .setColor(this.client.color.red)
-                .setDescription(
-                  `The queue is too long. The maximum length is ${client.config.maxQueueSize} songs.`
-                ),
-            ],
-          });
-        player.queue.push(track);
-        await player.isPlaying();
-        ctx.sendMessage({
-          embeds: [
-            embed
-              .setColor(this.client.color.main)
-              .setDescription(
-                `Added [${res.data.info.title}](${res.data.info.uri}) to the queue.`
-              ),
-          ],
-        });
-        break;
-      }
-      case LoadType.PLAYLIST: {
-        if (res.data.tracks.length > client.config.maxPlaylistSize)
-          return await ctx.sendMessage({
-            embeds: [
-              embed
-                .setColor(this.client.color.red)
-                .setDescription(
-                  `The playlist is too long. The maximum length is ${client.config.maxPlaylistSize} songs.`
-                ),
-            ],
-          });
-        for (const track of res.data.tracks) {
-          const pl = player.buildTrack(track, ctx.author);
-          if (player.queue.length > client.config.maxQueueSize)
-            return await ctx.sendMessage({
-              embeds: [
-                embed
-                  .setColor(this.client.color.red)
-                  .setDescription(
-                    `The queue is too long. The maximum length is ${client.config.maxQueueSize} songs.`
-                  ),
-              ],
-            });
-          player.queue.push(pl);
+        case LoadType.ERROR: return d.aoiError.fnError(d, "custom", {}, `There was an error while searching.`);
+        case LoadType.EMPTY: return d.aoiError.fnError(d, "custom", {}, `There were no results found.`);
+        case LoadType.TRACK: {
+            const track = player.buildTrack(res.data, d.author);
+            if (player.queue.length > d.client.musicOptions.maxQueueSize)
+                return d.aoiError.fnError(d, "custom", {}, `The queue length is to long. The maximum length is ${d.client.musicOptions.maxQueueSize} songs`); 
+            player.queue.push(track);
+            await player.isPlaying();
+            break;
         }
-        await player.isPlaying();
-        ctx.sendMessage({
-          embeds: [
-            embed
-              .setColor(this.client.color.main)
-              .setDescription(
-                `Added ${res.data.tracks.length} songs to the queue.`
-              ),
-          ],
-        });
-        break;
-      }
-      case LoadType.SEARCH: {
-        const track1 = player.buildTrack(res.data[0], ctx.author);
-        if (player.queue.length > client.config.maxQueueSize)
-          return await ctx.sendMessage({
-            embeds: [
-              embed
-                .setColor(this.client.color.red)
-                .setDescription(
-                  `The queue is too long. The maximum length is ${client.config.maxQueueSize} songs.`
-                ),
-            ],
-          });
-        player.queue.push(track1);
-        await player.isPlaying();
-        ctx.sendMessage({
-          embeds: [
-            embed
-              .setColor(this.client.color.main)
-              .setDescription(
-                `Added [${res.data[0].info.title}](${res.data[0].info.uri}) to the queue.`
-              ),
-          ],
-        });
-        break;
-      }
+        case LoadType.PLAYLIST: {
+            if (res.data.tracks.length > d.client.musicOptions.maxPlaylistSize)
+                return d.aoiError.fnError(d, "custom", {}, `The queue length is to long. The maximum length is ${d.client.musicOptions.maxPlaylistSize} songs`); 
+            for (const track of res.data.tracks) {
+                const pl = player.buildTrack(track, d.author);
+                if (player.queue.length > d.client.musicOptions.maxQueueSize)
+                    return d.aoiError.fnError(d, "custom", {}, `The queue length is to long. The maximum length is ${d.client.musicOptions.maxQueueSize} songs`); 
+                player.queue.push(pl);
+            }
+            await player.isPlaying();
+            break;
+        }
+        case LoadType.SEARCH: {
+            const track = player.buildTrack(res.data[0], d.author);
+            if (player.queue.length > d.client.musicOptions.maxQueueSize)
+                return d.aoiError.fnError(d, "custom", {}, `The queue length is to long. The maximum length is ${d.client.musicOptions.maxQueueSize} songs`); 
+            player.queue.push(track);
+            await player.isPlaying();
+            break;
+        }
     }
-
-
     
     return {
         code: d.util.setCode(data)
