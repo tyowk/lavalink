fab50e4f25d83847ae12f48122abdceb36edb045
Tyowk <tyowk.personal@gmail.com>
2024-11-13 19:30:58 +0700

    Create index.js

---
+++ src/lib/index.js
@@ -0,0 +1,8 @@
+const { Client } = require('discord.js');
+const { ShoukakuClient } = require('./Shoukaku.js');
+
+exports.Music = class Music extends ShoukakuClient {
+    constructor(client, options) {
+        
+    }
+}
