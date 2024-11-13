8c1c20fdf04b8e279d4f388b389047b5f6c1b38d
Tyowk <tyowk.personal@gmail.com>
2024-11-13 20:14:05 +0700

    Update Client.js

--- src/lib/classes/Client.js
+++ src/lib/classes/Client.js
@@ -21,7 +21,7 @@ exports.MusicClient = class Client extends Shoukaku {
         this.client = client;
         this.client.shoukaku = this;
         this.client.queue = new ClientQueue(this);
-        new CustomFunctions(this.client);
+        new CustomFunctions(this.client, options.debug);
         this.on('ready', (name, reconnected) => this.emit(reconnected ? 'reconnect' : 'connect', name));
     }
 }
