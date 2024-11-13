4b7dfc61a70da3c9921e1d683ab7bd8883149c5a
Tyowk <tyowk.personal@gmail.com>
2024-11-13 19:43:51 +0700

    Update Client.js

--- src/lib/classes/Client.js
+++ src/lib/classes/Client.js
@@ -3,7 +3,7 @@ const { Connectors, Shoukaku } = require('shoukaku');
 exports.Music = class Music extends Shoukaku {
     constructor(client, options) {
         if (!options || !options.nodes) throw new Error('There is no nodes provided to connect on!');
-        if (options.nodes) Array.isArray(options.nodes) ? options.nodes : [options.nodes];
+        if (options.nodes) options.nodes = Array.isArray(options.nodes) ? options.nodes : [options.nodes];
         super(new Connectors.DiscordJS(client), options.nodes, {
             moveOnDisconnect: false,
             resume: false,
