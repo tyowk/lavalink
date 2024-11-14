20f49b13a3382d897fd02885a4bd293cce51349d
Tyowk <tyowk.personal@gmail.com>
2024-11-14 23:29:46 +0700

    Update skipTrack.js

--- src/functions/skipTrack.js
+++ src/functions/skipTrack.js
@@ -1,6 +1,7 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    const player = d.client.queue.get(d.guild.id);
+    const [guildId] = data.inside.splits;
+    const player = d.client.queue.get(guildId ? guildId : d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
 
     if (player.queue.length === 0) return;
