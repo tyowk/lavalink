a0079527271939fc84a413b3e2cfa68b1f0b8d53
Tyowk <tyowk.personal@gmail.com>
2024-11-14 17:03:29 +0700

    Update Client.js

--- src/classes/Client.js
+++ src/classes/Client.js
@@ -10,10 +10,10 @@ exports.Client = class Client extends Shoukaku {
         if (!client) throw new Error('Client instance is not defined.');
         if (!options || !options.nodes) throw new Error('There is no nodes provided to connect on!');
         options.nodes = Array.isArray(options.nodes) ? options.nodes : [options.nodes];
-        if (!options.maxQueueSize) options.maxQueueSize = 100;
-        if (!options.maxPlaylistSize) options.maxPlaylistSize = 100;
-        if (!options.searchEngine) options.searchEngine = 'ytsearch';
-        if (!options.debug) options.debug = false;
+        options.maxQueueSize = options.maxQueueSize || 100;
+        options.maxPlaylistSize = options.maxPlaylistSize || 100;
+        options.searchEngine = options.searchEngine || 'ytsearch';
+        options.debug = options.debug || false;
         
         super(new Connectors.DiscordJS(client), options.nodes, {
             moveOnDisconnect: false,
@@ -22,15 +22,18 @@ exports.Client = class Client extends Shoukaku {
             reconnectTries: 2,
             restTimeout: 10000,
             userAgent: options.userAgent || 'NouJS',
-            nodeResolver: (nodes) => [...nodes.values()]
-                .filter(node => node.state === 2)
-                .sort((a, b) => a.penalties - b.penalties)
-                .shift(),
+            nodeResolver: (nodes) => {
+                return [...nodes.values()]
+                    .filter(node => node.state === 2)
+                    .sort((a, b) => a.penalties - b.penalties)
+                    .shift();
+            },
         });
+        
         this.#client = client;
         this.#client.shoukaku = this;
         this.#client.music = options;
-        
+
         this.cmds = {
             trackStart: new Collection(),
             trackEnd: new Collection(),
@@ -40,7 +43,7 @@ exports.Client = class Client extends Shoukaku {
             trackPaused: new Collection(),
             trackResumed: new Collection(),
         };
-        
+
         this.#client.music.events = [
             'trackStart',
             'trackEnd',
@@ -48,78 +51,68 @@ exports.Client = class Client extends Shoukaku {
             'trackPaused',
             'trackResumed',
             'queueStart',
-            'queueEnd'
+            'queueEnd',
         ];
-        
+
         this.#client.music.cmds = this.cmds;
         this.#client.queue = new ClientQueue(this.#client, options);
-        new CustomFunctions(this.#client, options.debug || false);
+        
+        new CustomFunctions(this.#client, options.debug);
         new MusicEvents(this.#client);
+
         this.on('ready', (name, reconnected) => this.emit(reconnected ? 'reconnect' : 'connect', name));
     }
 
-    async loadEvents(dir, debug = options.debug || false) {
+    async loadMusicEvents(dir, debug = false) {
         const loader = new LoadCommands(this.#client);
         await loader.load(this.cmds, dir, debug);
-        this.music.events.forEach(event => this.#bindEvents(event));
+        this.#client.music.events.forEach(event => this.#bindEvents(event));
     }
 
     #bindEvents(event) {
-        this.on(event, (...data) => {
+        this.on(event, async (...data) => {
             const player = data.shift();
             const dispatcher = data.pop();
-            
+
             this.cmds[event].forEach(async (cmd) => {
                 if (!cmd.__compiled__) {
                     let channel;
-
                     if (cmd.channel.startsWith("$")) {
                         const guildId = player.guildId;
-                        const guild = this.client.guilds.cache.get(guildId);
+                        const guild = this.#client.guilds.cache.get(guildId);
                         const channelId = dispatcher.channelId;
-                        const channelData = await this.client.functionManager.interpreter(
-                            this.client,
-                            {
-                                guild: guild,
-                                channel: this.client.channels.cache.get(channelId),
-                            },
+                        const channelData = await this.#client.functionManager.interpreter(
+                            this.#client,
+                            { guild, channel: this.#client.channels.cache.get(channelId) },
                             [],
                             { code: cmd.channel, name: "NameParser" },
                             undefined,
                             true,
                             undefined,
-                            {
-                                data: data[0],
-                                player: player,
-                            }
+                            { data: data[0], player: player }
                         );
                         channel = channelData?.code;
                     }
-                    const resolvedChannel = this.client.channels.cache.get(channel);
-                    return await this.client.functionManager.interpreter(
-                        this.client,
-                        {
-                            guild: this.client.guilds.cache.get(player.guildId),
-                            channel: resolvedChannel,
-                        },
+                    const resolvedChannel = this.#client.channels.cache.get(channel);
+                    await this.#client.functionManager.interpreter(
+                        this.#client,
+                        { guild: this.#client.guilds.cache.get(player.guildId), channel: resolvedChannel },
                         [],
                         cmd,
                         undefined,
                         false,
                         resolvedChannel,
-                        {
-                            data: data[0],
-                         }
+                        { data: data[0] }
                     );
+                } else {
+                    await cmd.__compiled__({
+                        bot: this.#client,
+                        client: this.#client,
+                        channel: this.#client.channels.cache.get(dispatcher.channelId),
+                        guild: this.#client.guilds.cache.get(player.guildId),
+                        player: player,
+                    });
                 }
-
-                return await cmd.__compiled__({
-                    bot: this.client,
-                    client: this.client,
-                    channel: this.client.channels.cache.get(dispatcher.channelId),
-                    guild: this.client.guilds.cache.get(player.guildId),
-                    player: player,
-                });
             });
             return event;
         });
