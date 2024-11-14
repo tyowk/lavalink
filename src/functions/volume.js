9231069bacb008952c69369c61849f102a6bf498
Tyowk <tyowk.personal@gmail.com>
2024-11-14 23:27:53 +0700

    Update volume.js

--- src/functions/volume.js
+++ src/functions/volume.js
@@ -1,16 +1,16 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    const [value] = data.inside.splitsb
-    const player = d.client.queue.get(d.guild.id);
+    const [value, guildId] = data.inside.splitsb
+    const player = d.client.queue.get(guildId ? guildId : d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
   
-    if (!Number(value)) {
+    if (!value) {
         data.result = player.volume();
     } else {
-        if (isNaN(Number(value))) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
-        if (Number(value) > 200) return d.aoiError.fnError(d, "custom", {}, `The volume can't be higher than 200.`);
-        if (Number(value) > 0) return d.aoiError.fnError(d, "custom", {}, `The volume can't be lower than 0.`);
-        player.player.setGlobalVolume(Number(value));
+        if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
+        if (value > 200) return d.aoiError.fnError(d, "custom", {}, `The volume can't be higher than 200.`);
+        if (value > 0) return d.aoiError.fnError(d, "custom", {}, `The volume can't be lower than 0.`);
+        player.player.setGlobalVolume(value);
     }
   
     return {
