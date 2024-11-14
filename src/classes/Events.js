a40a5c26a4191a8c9ade45e5e78f669bc3df5d37
Tyowk <tyowk.personal@gmail.com>
2024-11-14 14:33:25 +0700

    Update Events.js

--- src/classes/Events.js
+++ src/classes/Events.js
@@ -1,15 +1,10 @@
-const { CustomEvents } = require('aoi.js');
-const EventEmitter = require('events');
-
 exports.MusicEvents = class Events {
     constructor(client) {
-        this.events = new CustomEvent(client);
-        this.cmds = client.music.cmds;
-        client.shoukaku.on('trackStart', async () => await EventEmitter.emit('trackStart'));
+        client.shoukaku.on('trackStart', async () => );
         client.shoukaku.on('trackEnd', async (p, t, d) => await this.trackEnd(p, t, d));
-        client.shoukaku.on('queueStart', async () => await EventEmitter.emit('queueStart'));
+        client.shoukaku.on('queueStart', async () => );
         client.shoukaku.on('queueEnd', async (p, t, d) => await this.queueEnd(p, t, d));
-        client.shoukaku.on('socketClosed', async () => await EventEmitter.emit('socketClosed'));
+        client.shoukaku.on('socketClosed', async () => );
     }
 
     async trackEnd(player, track, dispatcher) {
@@ -32,68 +27,5 @@ exports.MusicEvents = class Events {
             dispatcher.previous = dispatcher.current;
             dispatcher.current = null;
         }
-    }
-
-    CustomMusicEvents() {
-        this.customTrackStart();
-        this.customTrackEnd();
-        this.customQueueStart();
-        this.customQueueEnd();
-        this.customSocketClosed();
-    }
-
-    customTrackStart() {
-        this.cmds.trackStart.forEach(event => {
-            this.events.command({
-                listen: event.type || 'trackStart',
-                channel: event.channel || '$channelId',
-                code: event.code,
-            });
-            this.events.listen('trackStart');
-        });
-    }
-
-    customTrackEnd() {
-        this.cmds.trackEnd.forEach(event => {
-            this.events.command({
-                listen: event.type || 'trackEnd',
-                channel: event.channel || '$channelId',
-                code: event.code,
-            });
-            this.events.listen('trackEnd');
-        });
-    }
-
-    customQueueStart() {
-        this.cmds.queueStart.forEach(event => {
-            this.events.command({
-                listen: event.type || 'queueStart',
-                channel: event.channel || '$channelId',
-                code: event.code,
-            });
-            this.events.listen('queueStart');
-        });
-    }
-
-    customQueueEnd() {
-        this.cmds.queueEnd.forEach(event => {
-            this.events.command({
-                listen: event.type || 'queueEnd',
-                channel: event.channel || '$channelId',
-                code: event.code,
-            });
-            this.events.listen('queueEnd');
-        });
-    }
-
-    customSocketClosed() {
-        this.cmds.socketClosed.forEach(event => {
-            this.events.command({
-                listen: event.type || 'socketClosed',
-                channel: event.channel || '$channelId',
-                code: event.code,
-            });
-            this.events.listen('socketClosed');
-        });
     }
 }
