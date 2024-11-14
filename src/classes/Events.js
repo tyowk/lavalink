f1ead05548cd78c293e71e1f3a2031cbca720630
Tyowk <tyowk.personal@gmail.com>
2024-11-14 10:06:14 +0700

    Update Events.js

--- src/classes/Events.js
+++ src/classes/Events.js
@@ -1,6 +1,10 @@
 exports.MusicEvents = class Events {
     constructor(client) {
         this.client = client;
+        this.client.shoukaku.on('trackStart', (player, track, dispatcher) => this.trackStart(player, track, dispatcher));
+        this.client.shoukaku.on('trackEnd', (player, track, dispatcher) => this.trackStart(player, track, dispatcher));
+        this.client.shoukaku.on('queueStart', (player, track, dispatcher) => this.queueStart(player, track, dispatcher));
+        this.client.shoukaku.on('queueEnd', (player, track, dispatcher) => this.queueEnd(player, track, dispatcher));
     }
 
     trackStart(player, track, dispatcher) {
