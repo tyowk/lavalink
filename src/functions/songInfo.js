94e3e466caf1c7a8510cd86bba0a8164dd8d1e45
Tyowk <tyowk.personal@gmail.com>
2024-11-14 11:27:22 +0700

    Update songInfo.js

--- src/functions/songInfo.js
+++ src/functions/songInfo.js
@@ -1,26 +1,32 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
-    let [query, index] = data.inside.splits;
-    if (!query) query = 'title';
+    let [type = 'title', index] = data.inside.splits;
 
     let player = d.client.queue.get(d.guild.id);
-    if (!player)
-       return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
-    const type = query.split('.');
-    const res = player.current.info;
-    if (!res)
-        return d.aoiError.fnError(d, "custom", {}, `There is no song are playing.`);
-    
-    if (type[0] === 'requester') {
-        result = res.requester[type[1]];;
-        data.result = result || 'undefined';
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    type = type.split('.');
+
+    if (index && !isNaN(index) && index > 0) {
+        const res = player.queue[(index - 1)]?.info;
+        if (res) {
+            if (type[0] === 'requester') {
+                data.result = res.requester[type[1]];
+            } else {
+                data.result = res[type[0]];
+            }
+        }
     } else {
-        const result = res[type]
-        data.result = result || 'undefined';
+        const res = player.current.info;
+        if (!res) return d.aoiError.fnError(d, "custom", {}, `There is no song currently playing.`);
+        if (type[0] === 'requester') {
+            data.result = res.requester[type[1]];
+        } else {
+            data.result = res[type[0]];
+        }
     }
-    
+
     return {
         code: d.util.setCode(data)
-    }
-}
+    };
+};
