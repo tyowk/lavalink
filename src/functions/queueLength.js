8a03427a53fb76cff041dc4e01a332fb65f011aa
Tyowk <tyowk.personal@gmail.com>
2024-11-14 22:48:44 +0700

    Create queueLength.js

---
+++ src/functions/queueLength.js
@@ -0,0 +1,12 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    
+    data.result = player.queue.length || 0;
+  
+    return {
+        code: d.util.setCode(data)
+    }
+}
