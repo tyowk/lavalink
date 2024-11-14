b8b589313e2457b6ac4cbbdfe9b91bdeb79b8b33
Tyowk <tyowk.personal@gmail.com>
2024-11-14 23:46:00 +0700

    Update volume.js

--- src/functions/volume.js
+++ src/functions/volume.js
@@ -1,6 +1,6 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    const [value] = data.inside.splitsb
+    const [value] = data.inside.splits;
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
   
