b758468aa22158dc23a49fc87838350e1745cb45
Tyowk <tyowk.personal@gmail.com>
2024-11-14 11:28:25 +0700

    Update hasPlayer.js

--- src/functions/hasPlayer.js
+++ src/functions/hasPlayer.js
@@ -1,6 +1,6 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    const [guildId = d.guild.id] = data.inside.splits;
+    const [guildId] = data.inside.splits;
     data.result = (d.client.queue.get(guildId ? guildId : d.guild.id)) ? true : false;
     return {
         code: d.util.setCode(data)
