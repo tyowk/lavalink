eeeeb97ea723ebb5f7cfb2853338b8f07740970c
Tyowk <tyowk.personal@gmail.com>
2024-11-14 23:54:31 +0700

    Update volume.js

--- src/functions/volume.js
+++ src/functions/volume.js
@@ -5,12 +5,12 @@ module.exports = async (d) => {
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
   
     if (!value) {
-        data.result = player.player.volume;
+        data.result = player.volume();
     } else {
         if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
         if (value > 200) return d.aoiError.fnError(d, "custom", {}, `The volume can't be higher than 200.`);
-        if (value > 0) return d.aoiError.fnError(d, "custom", {}, `The volume can't be lower than 0.`);
-        player.player.setGlobalVolume(value);
+        if (value < 0) return d.aoiError.fnError(d, "custom", {}, `The volume can't be lower than 0.`);
+        player.volume(value);
     }
   
     return {
