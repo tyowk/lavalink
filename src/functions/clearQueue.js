5db39c7803fc5549ce0e2a546b77485c4f156467
Tyowk <tyowk.personal@gmail.com>
2024-11-15 00:19:30 +0700

    Update clearQueue.js

--- src/functions/clearQueue.js
+++ src/functions/clearQueue.js
@@ -1,12 +1,8 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
-
-    if (player.queue.length === 0) return;
     player.queue = [];
-  
     return {
         code: d.util.setCode(data)
     }
