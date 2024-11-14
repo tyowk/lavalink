e54e5c05976355d43346a423738cbf2f82d590cb
Tyowk <tyowk.personal@gmail.com>
2024-11-14 11:50:12 +0700

    Update connect.js

--- src/functions/connect.js
+++ src/functions/connect.js
@@ -1,17 +1,20 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
+    let [voiceId] = data.inside.splits;
+    if (!voiceId) voiceId = d.member?.voice?.channel?.id;
+    const voiceChannel = await d.guild.channels.fetch(voiceId);
     
     let player = d.client.queue.get(d.guild.id);
     if (player) player.destroy();
   
     player = await d.client.queue.create(
         d.guild,
-        d.member?.voice?.channel,
+        voiceChannel,
         d.channel,
-        d.client.shoukaku.options.nodeResolver(d.client.shoukaku.nodes);
+        d.client.shoukaku.options.nodeResolver(d.client.shoukaku.nodes) // Ensure this resolves correctly
     );
   
     return {
         code: d.util.setCode(data)
-    }
+    };
 }
