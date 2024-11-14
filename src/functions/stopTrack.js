a72d1306ee737d0148f0e22bce8bb7781bb39867
Tyowk <tyowk.personal@gmail.com>
2024-11-14 11:38:25 +0700

    Update stopTrack.js

--- src/functions/stopTrack.js
+++ src/functions/stopTrack.js
@@ -3,6 +3,7 @@ module.exports = async (d) => {
     
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player.current) return d.aoiError.fnError(d, "custom", {}, `There is no song currently playing.`);
     
     player.queue = [];
     player.stop();
