6bc102196fe390a2666d47b2bb0b7fd5c8c3747f
Tyowk <tyowk.personal@gmail.com>
2024-11-14 22:55:39 +0700

    Create volume.js

---
+++ src/functions/volume.js
@@ -0,0 +1,19 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    const [value] = data.inside.splitsb
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+  
+    if (!value) {
+        data.result = player.volume;
+    } else {
+        if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
+        if (value > 200) return d.aoiError.fnError(d, "custom", {}, `The volume can't be higher than 200.`);
+        if (value > 0) return d.aoiError.fnError(d, "custom", {}, `The volume can't be lower than 0.`);
+        player.player.setGlobalVolume(value);
+    }
+  
+    return {
+        code: d.util.setCode(data)
+    }
+}
