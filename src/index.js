cbd1777127994dcda71f7af3366677478a0c9f86
Tyowk <tyowk.personal@gmail.com>
2024-11-14 17:15:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 const { Client } = require('./classes/Client.js');
+
 exports.MusicClient = Client;
+exports.Events = {
+    TrackStart: 'trackStart',
+    TrackEnd: 'trackEnd',
+    QueueStart: "queueStart",
+    QueueEnd: 'queueEnd',
+    TrackStuck: 'trackStuck',
+    TrackPaused: 'trackPaused',
+    TrackResumed: 'trackResumed'
+};
