b0047f0c94e2f1c462f07059c4e43f792d91ad59
Tyowk <tyowk.personal@gmail.com>
2024-11-14 14:34:52 +0700

    Update Client.js

--- src/classes/Client.js
+++ src/classes/Client.js
@@ -29,48 +29,9 @@ exports.Client = class Client extends Shoukaku {
         this.client = client;
         this.client.shoukaku = this;
         this.client.music = options;
-        
-        this.cmds = {
-            trackStart: new Collection(),
-            trackEnd: new Collection(),
-            queueStart: new Collection(),
-            queueEnd: new Collection(),
-            trackStuck: new Collection(),
-            socketClosed: new Collection(),
-        };
-
-        if (!this.client.music) this.client.music = {};
-        this.client.music.cmds = this.cmds;
         this.client.queue = new ClientQueue(this.client, options);
         new CustomFunctions(this.client, options.debug || false);
         new MusicEvents(this.client);
         this.on('ready', (name, reconnected) => this.emit(reconnected ? 'reconnect' : 'connect', name));
     }
-
-    trackStart(data) {
-        this.cmds.trackStart.set(this.cmds.trackStart.size, data);
-    }
-    
-    trackEnd(data) {
-        this.cmds.trackEnd.set(this.cmds.trackEnd.size, data);
-    }
-    
-    queueStart(data) {
-        this.cmds.queueStart.set(this.cmds.queueStart.size, data);
-    }
-    
-    queueEnd(data) {
-        this.cmds.queueEnd.set(this.cmds.queueEnd.size, data);
-    }
-    
-    trackStuck(data) {
-        this.cmds.trackStuck.set(this.cmds.trackStuck.size, data);
-    }
-    
-    socketClosed(data) {
-        this.cmds.socketClosed.set(this.cmds.socketClosed.size, data);
-    }
-
-    loadCommands(basePath, debug) {}
 }
-
