e72a9513934bbbb39801cec3823952217607fdd2
Tyowk <tyowk.personal@gmail.com>
2024-11-15 13:27:49 +0700

    Create maxQueueSize.js

---
+++ src/functions/maxQueueSize.js
@@ -0,0 +1,16 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    const [value] = data.inside.splits;
+    
+    if (!value) {
+        data.result = d.client.music?.maxQueueSize || 100;
+    } else {
+        if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
+        if (value < 1) return d.aoiError.fnError(d, "custom", {}, `The maximum queue size can't be lower than 1.`);
+        if (d.client.music?.maxQueueSize) d.client.music?.maxQueueSize = isNaN(value) ? 100  : value;
+    }
+  
+    return {
+        code: d.util.setCode(data)
+    }
+}
