cc6820ab562a26cda9ea25af816f8d986a0d47ab
Tyowk <tyowk.personal@gmail.com>
2024-11-15 13:42:58 +0700

    Update maxQueueSize.js

--- src/functions/maxQueueSize.js
+++ src/functions/maxQueueSize.js
@@ -3,11 +3,11 @@ module.exports = async (d) => {
     const [value] = data.inside.splits;
     
     if (!value) {
-        data.result = d.client.music?.maxQueueSize || 100;
+        data.result = d.client.music.maxQueueSize || 100;
     } else {
         if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
         if (value < 1) return d.aoiError.fnError(d, "custom", {}, `The maximum queue size can't be lower than 1.`);
-        d.client.music?.maxQueueSize = isNaN(value) ? 100 : value;
+        d.client.music.maxQueueSize = isNaN(value) ? 100 : value;
     }
   
     return {
