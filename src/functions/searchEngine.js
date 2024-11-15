31f59c03a5bf5dbed53949c233c5b37f9d512d13
Tyowk <tyowk.personal@gmail.com>
2024-11-15 13:44:11 +0700

    Update searchEngine.js

--- src/functions/searchEngine.js
+++ src/functions/searchEngine.js
@@ -3,10 +3,9 @@ module.exports = async (d) => {
     const [value] = data.inside.splits;
     
     if (!value) {
-        data.result = d.client.music?.searchEngine || 'ytsearch';
+        data.result = d.client.music.searchEngine || 'ytsearch';
     } else {
         if (typeof value !== 'string') return d.aoiError.fnError(d, "custom", {}, `The type of value must be a string.`);
-        if (d.client.music?.searchEngine) d.client.music?.searchEngine = value || 'ytsearch';
     }
   
     return {
