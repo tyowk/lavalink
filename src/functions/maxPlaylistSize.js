319dab23dfa4531b984c68892758f7a6e5ad7f50
Tyowk <tyowk.personal@gmail.com>
2024-11-15 13:28:48 +0700

    Create maxPlaylistSize.js

---
+++ src/functions/maxPlaylistSize.js
@@ -0,0 +1,16 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    const [value] = data.inside.splits;
+    
+    if (!value) {
+        data.result = d.client.music?.maxPlaylistSize || 100;
+    } else {
+        if (isNaN(value)) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
+        if (value < 1) return d.aoiError.fnError(d, "custom", {}, `The maximum playlist size can't be lower than 1.`);
+        if (d.client.music?.maxPlaylistSize) d.client.music?.maxPlaylistSize = isNaN(value) ? 100 : value;
+    }
+  
+    return {
+        code: d.util.setCode(data)
+    }
+}
