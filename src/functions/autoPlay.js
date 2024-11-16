ade4841083e40660a5e08321715ffc9074a6a7e0
Tyowk <tyowk.personal@gmail.com>
2024-11-16 11:54:37 +0700

    Update autoPlay.js

--- src/functions/autoPlay.js
+++ src/functions/autoPlay.js
@@ -9,6 +9,7 @@ module.exports = async (d) => {
     if (!value) {
         data.result = player.autoplay;
     } else {
+        if (!player.current) return d.aoiError.fnError(d, "custom", {}, `There is no song currently playing.`);
         player.setAutoplay((value && value == 'true') ? true : false, type);
     }
   
