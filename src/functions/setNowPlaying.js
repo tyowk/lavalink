fbe0258682852445d9181de07bcad7a18499fdb5
Tyowk <tyowk.personal@gmail.com>
2024-11-16 08:04:02 +0700

    Update setNowPlaying.js

--- src/functions/setNowPlaying.js
+++ src/functions/setNowPlaying.js
@@ -1,23 +1,18 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    let [messageId] = data.inside.splits;
-    if (!voiceId) voiceId = d.member?.voice?.channel?.id;
-    if (!voiceId) return d.aoiError.fnError(d, "custom", {}, `You are not connected to any voice channels.`);
-    const voiceChannel = await d.guild.channels.fetch(voiceId);
-    if (voiceChannel.type !== ChannelType.GuildVoice && voiceChannel.type !== ChannelType.GuildStageVoice)
-        return d.aoiError.fnError(d, "custom", {}, `Invalid channel type: ${voiceChannel.type}, must be voice or stage channel!`);
+    if (data.err) return d.aoiError
+    const [messageId] = data.inside.splits;
     
-    let player = d.client.queue.get(d.guild.id);
-    if (player) player.destroy();
-  
-    player = await d.client.queue.create(
-        d.guild,
-        voiceChannel,
-        d.channel,
-        d.client.shoukaku.options.nodeResolver(d.client.shoukaku.nodes)
-    );
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+
+    if (!value) {
+        data.result = player.autoplay;
+    } else {
+        player.autoplay = (value && value == 'true') ? true : false;
+    }
   
     return {
         code: d.util.setCode(data)
-    };
-};
+    }
+}
