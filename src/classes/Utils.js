a317cbd76126d18122385045701b870c7341c1cf
Tyowk <tyowk.personal@gmail.com>
2024-11-16 19:49:24 +0700

    Update Utils.js

--- src/classes/Utils.js
+++ src/classes/Utils.js
@@ -24,15 +24,6 @@ module.exports = class Utils {
         return chunked_arr;
     }
     
-    static formatBytes(bytes, decimals = 2) {
-        if (bytes === 0) return "0 Bytes";
-        const k = 1024;
-        const dm = decimals < 0 ? 0 : decimals;
-        const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-        const i = Math.floor(Math.log(bytes) / Math.log(k));
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-    }
-    
     static parseTime(string) {
         const time = string.match(/([0-9]+[d,h,m,s])/g);
         if (!time) return 0;
