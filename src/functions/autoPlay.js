e3879b9905464352a93a7cb7e018d0ba6920edce
Tyowk <tyowk.personal@gmail.com>
2024-11-16 14:43:31 +0700

    Update autoPlay.js

--- src/functions/autoPlay.js
+++ src/functions/autoPlay.js
@@ -2,12 +2,13 @@ module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     let [value, type] = data.inside.splits;
     type = type ? type : d.client.music.searchEngine;
+    
     const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
     
     if (!value) {
         data.result = player.autoplay || false;
     } else {
-        if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
         if (!player.current) return d.aoiError.fnError(d, "custom", {}, `There is no song currently playing.`);
         await player.setAutoplay((value && value == 'true') ? true : false, type);
     }
