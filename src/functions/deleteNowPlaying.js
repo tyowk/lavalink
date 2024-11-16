1564c041411ae4ad97141d3319fb7e6c57d0c97a
Tyowk <tyowk.personal@gmail.com>
2024-11-16 08:23:05 +0700

    Update deleteNowPlaying.js

--- src/functions/deleteNowPlaying.js
+++ src/functions/deleteNowPlaying.js
@@ -5,7 +5,7 @@ module.exports = async (d) => {
 
     const msg = player.nowPlayingMessage;
     if (!msg) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is exists.`);
-    if (!msg.deletable || !msg.author.id) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is deletable and sended by the bot.`);
+    if (!msg.deletable || msg.author.id !== d.client.user.id) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is deletable and sended by the bot.`);
     msg.delete().catch(() => {});
     
     return {
