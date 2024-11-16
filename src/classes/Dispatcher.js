e28092e269bd6393c74bc6565aa4be1fb5b41c6f
Tyowk <tyowk.personal@gmail.com>
2024-11-16 14:43:01 +0700

    Update Dispatcher.js

--- src/classes/Dispatcher.js
+++ src/classes/Dispatcher.js
@@ -19,13 +19,13 @@ module.exports = class Dispatcher {
         this.currentVolume = 100;
         this.nowPlayingMessage = null;
         this.player
-            .on('start', () => {
-                if (this.queue.length) this.client.shoukaku.emit('queueStart', this.player, this.current, this);
+            .on('start', async () => {
+                if (this.queue.length) await this.client.shoukaku.emit('queueStart', this.player, this.current, this);
                 this.client.shoukaku.emit('trackStart', this.player, this.current, this);
             })
-            .on('end', () => {
+            .on('end', async () => {
+                await this.client.shoukaku.emit('trackEnd', this.player, this.current, this);
                 if (!this.queue.length) this.client.shoukaku.emit('queueEnd', this.player, this.current, this);
-                this.client.shoukaku.emit('trackEnd', this.player, this.current, this);
             })
             .on('stuck', () => {
                 this.client.shoukaku.emit('trackStuck', this.player, this.current)
