44c9bb30b78529068e6b2fbf6c035e710344a33c
Tyowk <tyowk.personal@gmail.com>
2024-11-16 12:23:59 +0700

    Update queueLength.js

--- src/functions/queueLength.js
+++ src/functions/queueLength.js
@@ -1,7 +1,7 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     const player = d.client.queue.get(d.guild.id);
-    data.result = player.queue?.length || 0;
+    data.result = player?.queue?.length || 0;
     return {
         code: d.util.setCode(data)
     }
