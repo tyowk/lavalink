ba0e2893a33fd8e544924e7c7fb256f363d731f2
Tyowk <tyowk.personal@gmail.com>
2024-11-16 20:14:40 +0700

    Create seek.js

---
+++ src/functions/seek.js
@@ -0,0 +1,17 @@
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    let [time] = data.inside.splits;
+    
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+
+    time = d.client.utils.parseTime(time.addBrackets());
+    if (!time) return d.aoiError.fnError(d, "custom", {}, `Invalid time format.`);
+    
+    player.seek(time);
+  
+    return {
+        code: d.util.setCode(data)
+    }
+};
