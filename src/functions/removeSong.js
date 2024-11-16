89d8e9029eb89877552088131afc054019f365a9
Tyowk <tyowk.personal@gmail.com>
2024-11-16 11:48:26 +0700

    Update removeSong.js

--- src/functions/removeSong.js
+++ src/functions/removeSong.js
@@ -1,47 +1,19 @@
-
-
-
-
-
-if (!player.queue.length)
-      return await ctx.sendMessage({
-        embeds: [
-          embed
-            .setColor(this.client.color.red)
-            .setDescription("There are no songs in the queue."),
-        ],
-      });
-    if (isNaN(Number(args[0])))
-      return await ctx.sendMessage({
-        embeds: [
-          embed
-            .setColor(this.client.color.red)
-            .setDescription("That is not a valid number."),
-        ],
-      });
-    if (Number(args[0]) > player.queue.length)
-      return await ctx.sendMessage({
-        embeds: [
-          embed
-            .setColor(this.client.color.red)
-            .setDescription("That is not a valid number."),
-        ],
-      });
-    if (Number(args[0]) < 1)
-      return await ctx.sendMessage({
-        embeds: [
-          embed
-            .setColor(this.client.color.red)
-            .setDescription("That is not a valid number."),
-        ],
-      });
-    player.remove(Number(args[0]) - 1);
-    return await ctx.sendMessage({
-      embeds: [
-        embed
-          .setColor(this.client.color.main)
-          .setDescription(
-            `Removed song number ${Number(args[0])} from the queue`
-          ),
-      ],
-    });
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    const [index] = data.inside.splits;
+    
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+      
+    if (!player.queue.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in the queue.`);
+    if (isNaN(Number(index))) return d.aoiError.fnError(d, "custom", {}, `That is not a valid number.`);
+    if (Number(index) > player.queue.length) return d.aoiError.fnError(d, "custom", {}, `That is not a valid number.`);
+    if (Number(index) < 1) return d.aoiError.fnError(d, "custom", {}, `That is not a valid number.`);
+      
+    player.remove(Number(index) - 1);
+    
+    return {
+        code: d.util.setCode(data)
+    }
+}
