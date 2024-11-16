f6bc1a9caa00f12bf9cd2c35b1eacde3767224c0
Tyowk <tyowk.personal@gmail.com>
2024-11-16 08:06:47 +0700

    Update setNowPlaying.js

--- src/functions/setNowPlaying.js
+++ src/functions/setNowPlaying.js
@@ -1,16 +1,16 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    if (data.err) return d.aoiError
+    if (data.err) return d.error(data.err);
     const [messageId] = data.inside.splits;
+    if (!messageId) return d.aoiError.fnError(d, "custom", {}, `Please give a valid message id.`);
     
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
 
-    if (!value) {
-        data.result = player.autoplay;
-    } else {
-        player.autoplay = (value && value == 'true') ? true : false;
-    }
+    const chn = await d.guild.channels.cache.get(player.channelId);
+    const msg = await chn.messages.cache.get(messageId);
+    if (!msg) return d.aoiError.fnError(d, "custom", {}, `Please give a valid message id.`);
+    
   
     return {
         code: d.util.setCode(data)
