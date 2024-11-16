a6ab8b1aeb2f3bb7459ecc2d4d10688cecdc4885
Tyowk <tyowk.personal@gmail.com>
2024-11-16 20:20:21 +0700

    Create shuffleQueue.js

---
+++ src/functions/shuffleQueue.js
@@ -0,0 +1,12 @@
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player.queue.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in the queue.`);
+    
+    player.setShuffle(true);
+  
+    return {
+        code: d.util.setCode(data)
+    }
+};
