cdd0b4d1492c78a97cfb9806f924d0167c3b9524
Tyowk <tyowk.personal@gmail.com>
2024-11-16 11:55:05 +0700

    Update resumeTrack.js

--- src/functions/resumeTrack.js
+++ src/functions/resumeTrack.js
@@ -5,6 +5,7 @@ module.exports = async (d) => {
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
 
+    if (!player.current) return d.aoiError.fnError(d, "custom", {}, `There is no song currently playing.`);
     if (player.paused) { player.pause() }
     else { return d.aoiError.fnError(d, "custom", {}, `The player is not paused.`) }
   
