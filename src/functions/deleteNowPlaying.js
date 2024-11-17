bc9996a81fc69036ed10f8101317e51bd04129dc
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:19:40 +0700

    Update deleteNowPlaying.js

--- src/functions/deleteNowPlaying.js
+++ src/functions/deleteNowPlaying.js
@@ -1,9 +1,13 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
 
-    const msg = player.nowPlayingMessage;
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+    
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
+
+    const msg = player.nowPlaying;
     if (!msg) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is exists.`);
     if (!msg.deletable || msg.author.id !== d.client.user.id) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is deletable and sended by the bot.`);
     await msg.delete().catch(() => {});
