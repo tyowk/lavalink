669f92f868cdee01b6a5e8758b1b277f28135396
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:28:32 +0700

    Update leaveVc.js

--- src/functions/leaveVc.js
+++ src/functions/leaveVc.js
@@ -1,6 +1,10 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [guildId] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+    
     const player = d.client.queue.get(guildId ? guildId : d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
     
