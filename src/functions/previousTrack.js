566b7c264c1d95d230f213e05a38326093e7701d
Tyowk <tyowk.personal@gmail.com>
2024-11-17 14:10:16 +0700

    Update previousTrack.js

--- src/functions/previousTrack.js
+++ src/functions/previousTrack.js
@@ -3,7 +3,7 @@ module.exports = (d) => {
     const player = d.client.queue.get(d.guild.id);
   
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
-    if (!player.history.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in history`.);
+    if (!player.previous) return d.aoiError.fnError(d, "custom", {}, `There is no previous track.`.);
     
     player.previousTrack();
   
