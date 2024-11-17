15b703f0e525fec79b9c747665151510127688d1
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:25:16 +0700

    Update hasPlayer.js

--- src/functions/hasPlayer.js
+++ src/functions/hasPlayer.js
@@ -1,6 +1,9 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [guildId] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     data.result = (d.client.queue.get(guildId ? guildId : d.guild.id)) ? true : false;
     
