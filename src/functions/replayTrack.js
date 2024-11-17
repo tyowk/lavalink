129f303daac33d195fb492a57676a3b055143143
Tyowk <tyowk.personal@gmail.com>
2024-11-17 13:58:19 +0700

    Update replayTrack.js

--- src/functions/replayTrack.js
+++ src/functions/replayTrack.js
@@ -1,5 +1,5 @@
+module.exports = (d) => {
     const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
 
