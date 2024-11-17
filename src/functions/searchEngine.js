3409cb25884472a2baedb14e58d9a3c3a1bc5c82
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:38:58 +0700

    Update searchEngine.js

--- src/functions/searchEngine.js
+++ src/functions/searchEngine.js
@@ -1,6 +1,9 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [value] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     if (!value) {
         data.result = d.client.music.searchEngine || 'ytsearch';
