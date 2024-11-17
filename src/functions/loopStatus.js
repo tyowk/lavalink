e1f0316d5a08a4a25c000faedffff00afa9f8514
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:29:48 +0700

    Update loopStatus.js

--- src/functions/loopStatus.js
+++ src/functions/loopStatus.js
@@ -1,9 +1,12 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [value] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
 
     data.result = player.loop;
   
