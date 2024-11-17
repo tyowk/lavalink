c3e023978d35e20b3573454dff00ad38e0a266e5
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:37:45 +0700

    Update removeSong.js

--- src/functions/removeSong.js
+++ src/functions/removeSong.js
@@ -7,7 +7,7 @@ module.exports = (d) => {
     if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
       
     if (!player.queue.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in the queue.`);
     if (isNaN(Number(index))) return d.aoiError.fnError(d, "custom", {}, `That is not a valid number.`);
