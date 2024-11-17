3dc19bc11634586144860be49094de6ce5d989ac
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:40:38 +0700

    Update skipTo.js

--- src/functions/skipTo.js
+++ src/functions/skipTo.js
@@ -2,9 +2,12 @@ module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
     let [index] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
     
     if (!player.queue.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in the queue.`);
     if (isNaN(Number(index))) return d.aoiError.fnError(d, "custom", {}, `Please provide a valid number.`);
