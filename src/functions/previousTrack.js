aedfa4840af8c038552db244a487925afa033f7c
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:34:12 +0700

    Update previousTrack.js

--- src/functions/previousTrack.js
+++ src/functions/previousTrack.js
@@ -1,7 +1,10 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const player = d.client.queue.get(d.guild.id);
-  
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+    
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
     if (!player.previous) return d.aoiError.fnError(d, "custom", {}, `There is no previous track.`);
     
