6dc020b7fb26b891b989e1b9be14201d818e9512
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:39:56 +0700

    Update setNowPlaying.js

--- src/functions/setNowPlaying.js
+++ src/functions/setNowPlaying.js
@@ -3,9 +3,12 @@ module.exports = (d) => {
     if (data.err) return d.error(data.err);
     const [messageId] = data.inside.splits;
     if (!messageId) return d.aoiError.fnError(d, "custom", {}, `Please give a valid message id.`);
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
 
     const chn = d.guild.channels.cache.get(player.channelId);
     if (!chn) return d.aoiError.fnError(d, "custom", {}, `Invalid channel, please make sure the channel is exists and the bot can access the channel.`);
@@ -14,7 +17,7 @@ module.exports = (d) => {
     if (!msg) return d.aoiError.fnError(d, "custom", {}, `Please give a valid message id.`);
     
     if (!msg.deletable || msg.author.id !== d.client.user.id) return d.aoiError.fnError(d, "custom", {}, `Invalid message, please make sure the message is deletable and sended by the bot.`);
-    player.nowPlayingMessage = msg;
+    player.nowPlaying = msg;
     
     return {
         code: d.util.setCode(data)
