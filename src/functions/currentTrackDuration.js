4f9126cc54be956b551221d6486935ed07f67beb
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:15:04 +0700

    Update currentTrackDuration.js

--- src/functions/currentTrackDuration.js
+++ src/functions/currentTrackDuration.js
@@ -1,6 +1,9 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [humanize = 'false'] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
