43374a6f27ebede27a976eb83ae13bbf293d5874
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:41:47 +0700

    Update songInfo.js

--- src/functions/songInfo.js
+++ src/functions/songInfo.js
@@ -3,9 +3,12 @@ module.exports = (d) => {
     let [type, index] = data.inside.splits;
     if (!type) type = 'title';
     type = type.split('.');
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
-    let player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    const player = d.client.queue.get(d.guild.id);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
 
     if (index && !isNaN(index) && index > 0) {
         const res = player.queue[(index - 1)]?.info;
