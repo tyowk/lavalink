e0b4eee9e1db536057107afe9eb5581a08801cad
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:39:19 +0700

    Update seek.js

--- src/functions/seek.js
+++ src/functions/seek.js
@@ -2,6 +2,9 @@ module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
     let [time] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
     if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
