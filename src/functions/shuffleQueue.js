fe89bf28e95ac15ef678c6dda915e739537cb184
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:40:17 +0700

    Update shuffleQueue.js

--- src/functions/shuffleQueue.js
+++ src/functions/shuffleQueue.js
@@ -1,7 +1,10 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+    
     const player = d.client.queue.get(d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
     if (!player.queue.length) return d.aoiError.fnError(d, "custom", {}, `There is no songs in the queue.`);
     
     player.setShuffle(true);
