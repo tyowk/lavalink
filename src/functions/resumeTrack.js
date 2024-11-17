c1d121f6a9206a41eb73f46748775012001736ee
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:38:37 +0700

    Update resumeTrack.js

--- src/functions/resumeTrack.js
+++ src/functions/resumeTrack.js
@@ -1,7 +1,5 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
-    const [value] = data.inside.splits;
-
     const manager = d.client.shoukaku;
     if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
