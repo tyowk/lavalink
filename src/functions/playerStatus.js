bfdbf7767ba840d7d1b3f8618305dc8afebc36f7
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:33:53 +0700

    Update playerStatus.js

--- src/functions/playerStatus.js
+++ src/functions/playerStatus.js
@@ -1,5 +1,8 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     const player = d.client.queue.get(d.guild.id);
     
     if (!player) {
