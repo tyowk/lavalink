60a5796a2f0143af4e94a5604c443eaeea3dd217
Tyowk <tyowk.personal@gmail.com>
2024-11-17 18:24:58 +0700

    Update disconnect.js

--- src/functions/disconnect.js
+++ src/functions/disconnect.js
@@ -1,8 +1,12 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const [guildId] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+    
     const player = d.client.queue.get(guildId ? guildId : d.guild.id);
-    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild!`);
+    if (!player) return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
     
     player.destroy();
   
