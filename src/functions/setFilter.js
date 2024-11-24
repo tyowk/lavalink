63e2b138cd1fccca70779be6f6db7dee961e8113
Tyowk <tyowk.personal@gmail.com>
2024-11-24 08:22:04 +0700

    Update setFilter.js

--- src/functions/setFilter.js
+++ src/functions/setFilter.js
@@ -1,6 +1,6 @@
 const { Filters } = require('../classes/Filters');
 
-module.exports = (d) => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
     let [value, custom] = data.inside.splits;
