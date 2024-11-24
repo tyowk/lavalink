934f2604a24eb866c9b31e411d3eff5dc056deec
Tyowk <tyowk.personal@gmail.com>
2024-11-24 08:14:03 +0700

    Update getFilter.js

--- src/functions/getFilter.js
+++ src/functions/getFilter.js
@@ -1,12 +1,15 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
+    const [separator = ', '] = data.inside.splits;
+    
     const manager = d.client.shoukaku;
     if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
     
     const player = d.client.queue.get(d.guild.id);
     if (!player)  return d.aoiError.fnError(d, "custom", {}, `There is no player for this guild.`);
 
-    data.result = player.filters;
+    if (player.filters.length > 0)
+        data.result = player.filters.join(separator);
     
     return {
         code: d.util.setCode(data)
