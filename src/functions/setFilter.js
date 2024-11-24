1766e6bef02739ed902fae1e3144288aca94379a
Tyowk <tyowk.personal@gmail.com>
2024-11-24 08:20:31 +0700

    Update setFilter.js

--- src/functions/setFilter.js
+++ src/functions/setFilter.js
@@ -15,17 +15,16 @@ module.exports = (d) => {
     if (value === 'custom') {
         if (!custom)  return d.aoiError.fnError(d, "custom", {}, `Please provide a valid custom filter.`);
         custom = JSON.parse(custom);
-        player.player?.update({
-            filters: custom
-        }, true);
+        await player.player?.update({ filters: {} }, true);
+        await player.player?.update({ filters: custom }, true);
+        player.filters = [];
         player.filters.push('custom');
     } else {
         const filter = Filters[value];
         if (!filter) return d.aoiError.fnError(d, "custom", {}, `Invalid filter provided: "${value}".`);
-        player.player?.update({
-            filters: filter
-        }, true);
-        player.filters.push(value);
+        player.player?.update({ filters: filter }, true);
+        if (value === 'clear') { player.filters = [] }
+        else { player.filters.push(value) }
     }
 
     return {
