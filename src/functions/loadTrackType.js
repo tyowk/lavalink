618b6188b5e422747a1a2b25d722c8e0b6605438
Tyowk <tyowk.personal@gmail.com>
2024-11-27 15:19:56 +0700

    Update loadTrackType.js

--- src/functions/loadTrackType.js
+++ src/functions/loadTrackType.js
@@ -1,6 +1,6 @@
 const { LoadType } = require('shoukaku');
 
-module.exports = (d) => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
     let [query, type] = data.inside.splits;
