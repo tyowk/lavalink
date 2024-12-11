5b85e70c5626d00575ce8ab7fdce3aa7e37df611
Tyowk <tyowk.personal@gmail.com>
2024-12-11 11:16:51 +0700

    Update Events.js

--- src/classes/Events.js
+++ src/classes/Events.js
@@ -22,20 +22,21 @@ exports.MusicEvents = class Events {
         client.on('debug', (name, reason) => client.emit('nodeRaw', name, reason));
     }
 
-    async trackEnd(player, track, dispatcher) {
+    async trackEnd(player, track, dispatcher, client) {
         dispatcher.previous = dispatcher.current;
         dispatcher.current = null;
-        if (dispatcher.loop === "repeat") dispatcher.queue.unshift(track);
-        if (dispatcher.loop === "queue") dispatcher.queue.push(track);
+        if (dispatcher.loop === 'repeat') dispatcher.queue.unshift(track);
+        if (dispatcher.loop === 'queue') dispatcher.queue.push(track);
+        if (dispatcher.autoplay === true) { await dispatcher.Autoplay(track); }
+        else { dispatcher.autoplay = false; };
         await dispatcher.play();
-        if (dispatcher.autoplay) { await dispatcher.Autoplay(track) }
     }
     
     async queueEnd(player, track, dispatcher) {
         if (dispatcher.loop === "repeat") dispatcher.queue.unshift(track);
         if (dispatcher.loop === "queue") dispatcher.queue.push(track);
-        if (dispatcher.autoplay === true) { await dispatcher.Autoplay(track);
-        } else { dispatcher.autoplay = false; }
+        if (dispatcher.autoplay === true) { await dispatcher.Autoplay(track) }
+        else { dispatcher.autoplay = false; };
         if (dispatcher.loop === "off") {
             dispatcher.previous = dispatcher.current;
             dispatcher.current = null;
