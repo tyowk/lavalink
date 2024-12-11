f6154e0a05137773dc8d6336c9f4c4461bf337b3
Tyowk <tyowk.personal@gmail.com>
2024-12-11 11:45:05 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ exports.Queue = require('./classes/Queue.js').Queue;
 exports.Dispatcher = require('./classes/Dispatcher.js').Dispatcher;
 exports.Events = require('./classes/Events.js').Events;
 exports.Functions = require('./classes/Functions.js').Functions;
-exports.Utils = require('./classes/Utils.js');
+exports.Utils = require('./classes/Utils.js').Utils;
