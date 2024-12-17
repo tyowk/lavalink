dc8bc27af7268c227bd2cdac7b5066412ef78636
Tyowk <tyowk.personal@gmail.com>
2024-12-17 17:28:13 +0700

    Update Manager.js

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -81,7 +81,7 @@ exports.Manager = class Manager extends Shoukaku {
 
     #bindEvents(event) {
         this.on(event, async (player, track, dispatcher) => {
-            const commands = this.client?.cmd?.[event] || this.cmd[event];
+            const commands = this.cmd[event];
             if (!commands) return;
             for (const cmd of commands.values()) {
                 const guild = this.client.guilds.cache.get(player?.guildId);
