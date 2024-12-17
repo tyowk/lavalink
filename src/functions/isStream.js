9c7e3fb05eba2a2d7a85fddaaf27f0440e8e3b85
Tyowk <tyowk.personal@gmail.com>
2024-12-17 19:18:01 +0700

    Create isStream.js

---
+++ src/functions/isStream.js
@@ -0,0 +1,18 @@
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    const [guildId] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+  
+    const player = d.client.queue.get(guildId ? guildId : d.guild.id);
+    if (!player) return d.client.returnCode(d, data);
+    
+    data.result = player?.current?.info?.isStream
+        ? true
+        : false;
+    
+    return {
+        code: d.util.setCode(data)
+    }
+}
