22f41f5e71a0f2326bc4be3bdc5937c5459cd224
Tyowk <tyowk.personal@gmail.com>
2024-12-17 19:18:45 +0700

    Update isPreviousExist.js

--- src/functions/isPreviousExist.js
+++ src/functions/isPreviousExist.js
@@ -6,6 +6,8 @@ module.exports = (d) => {
     if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
   
     const player = d.client.queue.get(guildId ? guildId : d.guild.id);
+    if (!player) return d.client.returnCode(d, data);
+    
     data.result = player?.previous ? true : false;
     
     return {
