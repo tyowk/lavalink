9b84d708db08b363e09ab1fbaf2a023b1ef8f752
Tyowk <tyowk.personal@gmail.com>
2024-12-17 19:15:51 +0700

    Create isSeekable.js

---
+++ src/functions/isSeekable.js
@@ -0,0 +1,14 @@
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    const [guildId] = data.inside.splits;
+
+    const manager = d.client.shoukaku;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Voice manager is not defined.`);
+  
+    const player = d.client.queue.get(guildId ? guildId : d.guild.id);
+    data.result = player?.current?.info?.isSeekable ? true : false;
+    
+    return {
+        code: d.util.setCode(data)
+    }
+}
